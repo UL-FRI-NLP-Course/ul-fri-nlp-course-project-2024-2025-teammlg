5f89add14328b08bf1a9c2c64c7f7dc5273802fd
Gorazd Gorup <gorupgorazd@gmail.com>
2025-05-12 17:57:15 +0200

    Update sling setup

--- code/sling_setup.sh
+++ code/sling_setup.sh
@@ -5,5 +5,10 @@ singularity exec ./containers/nlp-v1.sif pip install transformers==4.51
 singularity exec ./containers/nlp-v1.sif pip install scipy
 singularity exec ./containers/nlp-v1.sif pip install spacy[cuda12x]
 singularity exec ./containers/nlp-v1.sif pip install fsspec==2025.3.0
+singularity exec ./containers/nlp-v1.sif pip install scikit-learn
+singularity exec ./containers/nlp-v1.sif pip install pytextrank
+singularity exec ./containers/nlp-v1.sif pip install rouge_score
+singularity exec ./containers/nlp-v1.sif pip install beautifulsoup4
+singularity exec ./containers/nlp-v1.sif pip install wikipedia
 mkdir logs
 sbatch ~/ul-fri-nlp-course-project-2024-2025-teammlg/run-slurm.sh
