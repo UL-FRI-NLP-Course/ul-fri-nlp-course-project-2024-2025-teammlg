1020114cd96f89be50642d44ddc4c4706c324545
Gorazd Gorup <gorupgorazd@gmail.com>
2025-05-16 22:45:11 +0200

    Bruh

--- code/sling-run.sh
+++ code/sling-run.sh
@@ -2,9 +2,13 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
 #SBATCH --partition=gpu
 #SBATCH --time=00:30:00
-#SBATCH --output=logs/sling-nlp-showcase-%J.out
-#SBATCH --error=logs/sling-nlp-showcase-%J.err
-#SBATCH --job-name="SLING NLP showcase"
+#SBATCH --output=logs/test-%J.out
+#SBATCH --error=logs/test-%J.err
+#SBATCH --job-name="gg6898 test"
+
+srun singularity exec --nv ./containers/nlp.sif python \
+    ul-fri-nlp-course-project-2024-2025-teammlg/code/tests/transformers_rag.py
+
