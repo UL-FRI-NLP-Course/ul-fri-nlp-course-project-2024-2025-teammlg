a63621fe167140732413afdfcf5aa397055bda2d
Gorazd Gorup <gorupgorazd@gmail.com>
2025-05-26 12:48:41 +0200

    Code v2: model

--- code_v2/model.py
+++ code_v2/model.py
@@ -1,6 +1,45 @@
-# TODO: Model base class
-"""
-Should handle memory, should expose methods that are implemented in both models
-    - Function calling method
-    - Prompt answering
-"""
+from abc import ABC, abstractmethod
+from typing import List, TypedDict
+
+
+class ChatElement(TypedDict):
+    role: str
+    content: str
+
+
+class Model(ABC):
+    def __init__(self, uses_memory: bool = False):
+        self.uses_memory = uses_memory
+        self.memory: List[ChatElement] = []
+
+    def save_to_memory(self, content: str, role: str, to_back: bool = False):
+        element = { "role": role, "content": content }
+        if to_back:
+            self.memory.insert(0, element)
+        else:
+            self.memory.append(element)
+
+    @abstractmethod
+    def answer_function_calling(self, prompt: str, tools: List) -> str:
+        """The model determines which function should be called to answer the prompt correctly.
+
+        Args:
+            prompt (str): The prompt to answer
+            tools (List): A list of functions that are available to call
+
+        Returns:
+            str: An answer containing function calls
+        """
+        pass
+
+    @abstractmethod
+    def answer_prompt(self, prompt: str) -> str:
+        """The model answers the question in prompt.
+
+        Args:
+            prompt (str): The prompt to answer
+
+        Returns:
+            str: An answer
+        """
+        pass
