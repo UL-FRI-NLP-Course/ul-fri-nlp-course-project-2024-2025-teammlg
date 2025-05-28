6809ec090a80f376a77d1d059dc3372916ae2e69
bajicluka01 <bajicluka01@gmail.com>
2025-05-28 13:34:26 +0200

    more qwen resuts

--- code/tests/llm_evaluation.sh
+++ code/tests/llm_evaluation.sh
@@ -2,11 +2,15 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:3
+#SBATCH --gres=gpu:2
 #SBATCH --partition=gpu
-#SBATCH --time=2:00:00
+#SBATCH --time=5:00:00
 #SBATCH --output=logs/test-%J.out
 #SBATCH --error=logs/test-%J.err
 #SBATCH --job-name="Evaluation LLM - Custom"
 
-srun singularity exec --writable-tmpfs --nv ~/containers/nlp-v1.sif python ./llm_evaluation.py $1 $2 $3 $4
+srun singularity exec --writable-tmpfs --nv ~/containers/nlp-v1.sif python ./llm_evaluation.py qwen_baseline Qwen/Qwen3-14B 1 5
+srun singularity exec --writable-tmpfs --nv ~/containers/nlp-v1.sif python ./llm_evaluation.py qwen_baseline Qwen/Qwen3-14B 6 10
+srun singularity exec --writable-tmpfs --nv ~/containers/nlp-v1.sif python ./llm_evaluation.py qwen_baseline Qwen/Qwen3-14B 11 15
+srun singularity exec --writable-tmpfs --nv ~/containers/nlp-v1.sif python ./llm_evaluation.py qwen_baseline Qwen/Qwen3-14B 16 20
+srun singularity exec --writable-tmpfs --nv ~/containers/nlp-v1.sif python ./llm_evaluation.py qwen_baseline Qwen/Qwen3-14B 30 35
